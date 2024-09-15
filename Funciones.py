ab68f1dfb91e8da1364e2fe9ace0e78da478da2f
Andrecs05 <andresfcontreras@icloud.com>
2024-09-15 10:43:04 -0500

    Automated update

--- Funciones.py
+++ Funciones.py
@@ -17,14 +17,33 @@ def validate_numeric_input(action, value_if_allowed):
     else:
         return True
     
-# Funcion para construir la viga y las cargas en una matrix 2xn donde beam[0][n] es el punto en la viga y beam[1][n] es la carga en cada punto
+# Funcion para construir la viga y las cargas en una matrix 2xn donde 
+# beam[0][n] es el punto en la viga
+# beam[1][n] es la carga en cada punto
+# beam[2][n] es el momento en cada punto
 
 def build_beam(len):
     step = 0.01
     dom = np.arange(0, len, step)
     loads = np.zeros_like(dom)
-    beam = np.array([dom, loads])
+    moment = np.zeros_like(dom)
+    beam = np.array([dom, loads,moment])
     return beam
+
+# Funcion para desactivar los botones de los apoyos
+
+def update_disable(inp,txt,type,apoyo1,apoyo2,apo1button,apo2button):
+    update(inp,txt)
+    if type.get() == 'Cantilever':
+        apoyo1.config(state='disabled')
+        apoyo2.config(state='disabled')
+        apo1button.config(state='disabled')
+        apo2button.config(state='disabled')
+    else:
+        apoyo1.config(state='normal')
+        apoyo2.config(state='normal')
+        apo1button.config(state='normal')
+        apo2button.config(state='normal')
 
 # Funcion que llama a la funcion build_beam y update
 
@@ -42,3 +61,5 @@ def update_supports(inp,txt,support):
         supp1 = int(float(inp.get())*100)
     elif support == 2:
         supp2 = int(float(inp.get())*100)
+
+
