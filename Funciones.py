882dcc75f58cc5d0450d41b6dc20d168912a7a9c
Andrecs05 <andresfcontreras@icloud.com>
2024-10-06 16:20:31 -0500

    Automated update

--- Funciones.py
+++ Funciones.py
@@ -11,7 +11,7 @@ def update(inp,txt):
 # Funcion para validar que la entrada sea numerica y solo dos decimales
 def validate_numeric_input(action, value_if_allowed):
     if action == '1': 
-        if re.match(r'^\d*\.?\d{0,2}$', value_if_allowed):
+        if re.match(r'^-?\d*\.?\d{0,2}$', value_if_allowed):
             return True
         else:
             return False
@@ -26,10 +26,10 @@ def validate_numeric_input(action, value_if_allowed):
 # beam[4][n] es el momento flector en cada punto
 
 def build_beam(len):
-    global scale
+    global scale, step
     step = 0.001
     scale = 1/step
-    dom = np.arange(0, len+0.01, step)
+    dom = np.arange(0, len, step)
     loads = np.zeros_like(dom)
     moment = np.zeros_like(dom)
     shear = np.zeros_like(dom)
@@ -41,13 +41,14 @@ def build_beam(len):
 
 def update_disable(inp,txt,type,apoyo1,apoyo2,apo1button,apo2button):
     update(inp,txt)
-    global beamtype 
+    global beamtype, supp1
     if type.get() == 'Cantilever':
         apoyo1.config(state='disabled')
         apoyo2.config(state='disabled')
         apo1button.config(state='disabled')
         apo2button.config(state='disabled')
         beamtype = 1
+        supp1 = 0
     else:
         apoyo1.config(state='normal')
         apoyo2.config(state='normal')
@@ -75,8 +76,8 @@ def update_supports(inp,txt,support):
 # Funcion que ubica las cargas
 
 def point_load(pos,mag):
-    global beam
-    x = int(float(pos.get())*scale)
+    global beam, scale
+    x = int(float(pos.get())*scale)-1
     beam[1][x] -= float(mag.get())
     return beam
 
@@ -86,40 +87,53 @@ def point_moment(pos,mag):
     global beam, scale
     x = int(float(pos.get())*scale)
     beam[2][x] += float(mag.get())
+    print(beam[2][x])
     return beam
 
 # Funcion que calcula las reacciones en los apoyos
 
 def calculate_reactions():
-    global beam, supp1, supp2, beamtype, R1, R2, scale
+    global beam, supp1, supp2, beamtype, R1, R2, MR, scale
     R1 = 0
     R2 = 0
     M1 = 0
     M2 = 0
+    MR = 0
     if beamtype == 1:
         for i in range(len(beam[0])):
+            MR -= beam[0][i]*beam[1][i]
             R1 -= beam[1][i]
+        print(R1,MR)
+        beam[1][supp1] = R1
+        beam[2][supp1] = MR
     elif beamtype == 2:
         for i in range(len(beam[0])):
-            M1 += (beam[0][i]-supp1/scale)*beam[1][i]
-            M2 += (beam[0][i]-supp2/scale)*beam[1][i]
+            M1 += (beam[0][i]-supp1/scale)*beam[1][i]+beam[2][i]
+            M2 += (beam[0][i]-supp2/scale)*beam[1][i]+beam[2][i]
             R1 = -M2/(supp1/scale-supp2/scale)
             R2 = -M1/(supp2/scale-supp1/scale)
-    beam[1][supp1] = R1
-    beam[1][supp2] = R2
+        print(R1,R2)
+        beam[1][supp1] = R1
+        beam[1][supp2] = R2
+   
     
-    print(R1,R2)
-    return R1, R2
+    
+    return R1, R2,  MR
 
 # Funcion que grafica la viga
 
 def plot_beam(beamgraph,figbeam):
-    global beam, supp1, supp2, scale
+    global beam, supp1, supp2, scale, beamtype
     figbeam.clear()
     ax = figbeam.add_subplot(111)
+    if beamtype == 1:
+        ax.plot([0,0],[5,-1],linewidth=5, color='gray', zorder=101)
+        
+    elif beamtype == 2:
+        ax.plot([supp1/scale,supp1/scale],[0,-1],linewidth=3, color='red')
+        ax.plot([supp2/scale,supp2/scale],[0,-1],linewidth=3, color='red')
+        
     ax.plot(beam[0], np.zeros_like(beam[0]), linewidth=5, zorder=100)
-    ax.plot([supp1/scale,supp1/scale],[0,-1],linewidth=3, color='red')
-    ax.plot([supp2/scale,supp2/scale],[0,-1],linewidth=3, color='red')
     ax.set_ylim(-1,5.5)
     ax.yaxis.set_visible(False) 
 
@@ -148,25 +162,43 @@ def plot_shear(sheargraph,figshear):
     for i in range(len(beam[0])-1):
         beam[3][i+1] = beam[3][i] + beam[1][i+1]
 
+    max_shear = np.max(np.abs(beam[3]))
+    max_shear_pos = beam[0][np.argmax(np.abs(beam[3]))]
+
     ax.axhline(0, color='black', linewidth=1)
     ax.plot([0,0],[0,beam[3][0]],color='blue')
     ax.plot(beam[0], beam[3],color='blue')
+    ax.title.set_text('Fuerzas cortantes')
+    ax.set_xlabel('Posicion (mm)')
+    ax.set_ylabel('Fuerza cortante (N)')
     ax.fill_between(beam[0], beam[3], 0, color='blue', alpha=0.5, interpolate=True) 
+    ax.plot(max_shear_pos, max_shear, 'r*', markersize=10, label=f'Fuerza cortante máxima: {max_shear} N')
+    ax.legend()
     sheargraph.draw()
+    plt.show()
 
 def plot_moment(momentgraph,figmoment):
-    global beam
+    global beam, scale, step
     figmoment.clear()
     ax = figmoment.add_subplot(111)
-    beam[4][0] = beam[1][0] 
+    beam[4][0] = step*beam[1][0] - beam[2][0]
     for i in range(len(beam[0])-1):
-        beam[4][i+1] = beam[4][i] + beam[3][i+1]
+        beam[4][i+1] = beam[4][i] + step*beam[3][i+1] - beam[2][i+1]
+
+    max_moment = -max(np.abs(beam[4]))
+    max_moment_pos = beam[0][np.argmax(np.abs(beam[4]))]
 
     ax.axhline(0, color='black', linewidth=1)
     ax.plot([0,0],[0,beam[4][0]], color='orange')
     ax.plot(beam[0], beam[4], color='orange')
+    ax.title.set_text('Momento flector')
+    ax.set_xlabel('Posicion (mm)')
+    ax.set_ylabel('Momento flector (Nmm)')
     ax.fill_between(beam[0], beam[4], 0, color='orange', alpha=0.5) 
+    ax.plot(max_moment_pos, max_moment, 'r*', markersize=10, label=f'Momento flexor máximo: {max_moment} Nmm')
+    ax.legend()
     momentgraph.draw()
+    plt.show()
 
     
         
