81f4daff8eee7d487400ff25aba33bc2e58e1822
Andrecs05 <andresfcontreras@icloud.com>
2024-10-20 18:59:15 -0500

    Automated update

--- update_repository.py
+++ update_repository.py
@@ -1,9 +1,17 @@
 import subprocess
+import os
 
 def commit_and_push_changes():
     try:
         # Define the repository directory
         repo_dir = r"C:\Users\andre\iCloudDrive\Uni\Tercero\Proyecto\Codigo_vigas"
+        #C:\Users\andre\iCloudDrive\Uni\Tercero\Proyecto\Codigo_vigas
+
+        # Check if the directory is a Git repository
+        if not os.path.isdir(os.path.join(repo_dir, ".git")):
+            print(f"The directory {repo_dir} is not a Git repository. Initializing...")
+            subprocess.run(["git", "init"], check=True, cwd=repo_dir)
+            print(f"Initialized empty Git repository in {repo_dir}")
 
         # Pull the latest changes from the remote repository
         subprocess.run(["git", "pull"], check=True, cwd=repo_dir)
